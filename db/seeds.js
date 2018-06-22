43206e4faca67e3e3a60905db41152c6faa0bde3
JTVictor <jtownsend@outlook.com>
2018-06-22 16:56:25 +0100

    DB seed file working

--- db/seeds.js
+++ db/seeds.js
@@ -1,11 +1,26 @@
-// const mongoose = require('mongoose');
-// mongoose.Promise = require('bluebird');
-// const User = require('../models/user');
-// const { dbGymBuddy } = require('../config/environment');
-//
-// mongoose.connect(bdGymBuddy, (err, db) =>
-//   db.dropDatabase();
-//
-//
-//
-// )
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+const User = require('../models/user');
+const { dbGymBuddy } = require('../config/environment');
+
+mongoose.connect(dbGymBuddy, (err, db) => {
+  db.dropDatabase();
+
+  User.create([{
+    firstName: 'Chris',
+    surName: 'Eubank',
+    age: 51,
+    address: '10 Downing Street, London, SW1A 2AA',
+    goals: 'Maintenance',
+    image: 'https://www.famousbirthdays.com/headshots/chris-eubank-2.jpg',
+    location: {
+      lat: 51.5034,
+      lng: 0.1276
+    },
+    description: 'I am looking for somebody to punch'
+  }])
+
+    .then(users => console.log(`${users.length} user(s) created`))
+    .catch(err => console.log(err))
+    .finally(() => mongoose.connection.close());
+});
--- models/user.js
+++ models/user.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema({
   age: { type: Number, required: true },
   address: { type: String, required: true },
   goals: { type: String, required: true },
+  image: { type: String, required: true },
   location: {
     lat: { type: Number, required: true },
     lng: { type: Number, required: true}
